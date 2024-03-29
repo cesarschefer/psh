f620ab91929e571074c35964888d90c7345f3274
Cesar <cesarschefer@gmail.com>
2024-03-29 13:40:58 -0300

    Format created_at field

--- backend/app/Http/Resources/StatisticResource.php
+++ backend/app/Http/Resources/StatisticResource.php
@@ -19,7 +19,7 @@ class StatisticResource extends JsonResource
             'uuid' => $this->player_uuid,
             'nickname' => $this->player->nickname,
             'profileImage' => $this->player->profile_image,
-            'creationDate' => $this->created_at,
+            'creationDate' => $this->created_at->format('Y-m-d H:i:s'),
             'score' => $this->score
         ];
     }
