6082ac640ec356cec8849b81ca014aa28036adba
Cesar <cesarschefer@gmail.com>
2024-03-29 22:35:22 -0300

    Adds backend exception handler

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,25 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Route not found',
+                    'data' => [],
+                    'status' => Response::HTTP_NOT_FOUND
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (Exception $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Server error',
+                    'data' => [],
+                    'status' => Response::HTTP_INTERNAL_SERVER_ERROR
+                ], 500);
+            }
+        });
     })->create();
